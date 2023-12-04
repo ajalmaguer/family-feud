0f15fafa511df7048720fd0681a39d1896c58291
AJ Almaguer <ajalmaguer@gmail.com>
2023-12-04 16:00:51 -0700

    basic question

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,33 @@
+type Question = {
+  text: string;
+  answers: Answer[];
+};
+
+type Answer = {
+  text: string;
+  points: number;
+  revealed: boolean;
+};
+
+const question: Question = {
+  text: 'What is the answer to life, the universe, and everything?',
+  answers: [
+    { text: 'A', points: 42, revealed: false },
+    { text: 'B', points: 34, revealed: false },
+  ],
+};
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">{question.text}</h1>
+      <ul>
+        {question.answers.map((question) => (
+          <li>
+            {question.text} - {question.points}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
