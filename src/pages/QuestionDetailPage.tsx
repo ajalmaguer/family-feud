b47f0ad9f502b1869c69e9df3c59857f0c48d4f8
AJ Almaguer <ajalmaguer@gmail.com>
2023-12-04 17:34:21 -0700

    reading data from firebase

--- src/pages/QuestionDetailPage.tsx
+++ src/pages/QuestionDetailPage.tsx
@@ -1,4 +1,7 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect } from 'react';
+import { foobar, questionsRef } from '../services/firebaseService';
+import { child, onValue } from 'firebase/database';
+import { useParams } from 'react-router-dom';
 
 const question: Question = {
   text: 'What is the answer to life, the universe, and everything?',
@@ -9,5 +12,177 @@ const question: Question = {
 };
 
 export const QuestionDetailPage: FunctionComponent<{}> = () => {
-  return <div>QuestionDetailPage works</div>;
+  const { id } = useParams<{ id: string }>();
+  useEffect(() => {
+    if (!id) {
+      console.log('do nothing');
+      return;
+    }
+    console.log('onMount');
+
+    const unsubscribe = onValue(child(questionsRef, id), (snapshot) => {
+      console.log('things changed | snapshot.val()', snapshot.val());
+      console.log(JSON.stringify(snapshot.val(), null, 2));
+    });
+
+    function onUnmount() {
+      console.log('on unMount');
+      unsubscribe();
+    }
+
+    return onUnmount;
+  }, [id]);
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">{question.text}</h1>
+      <ul>
+        {question.answers.map((answer, i) => (
+          <li
+            key={
+              i
+              // answer.id
+            }
+          >
+            {answer.text} - {answer.points}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const blah = {
+  '-MQiUSY-uKFItfxdkAti': {
+    answers: [
+      {
+        points: 45,
+        text: 'Cereal',
+      },
+      {
+        points: 21,
+        text: 'Oatmeal',
+      },
+      {
+        points: 12,
+        text: 'Pancakes',
+      },
+      {
+        points: 7,
+        text: 'Eggs',
+      },
+      {
+        points: 5,
+        text: 'Toast',
+      },
+      {
+        points: 3,
+        text: 'Bagel',
+      },
+      {
+        points: 3,
+        text: 'Yogurt',
+      },
+      {
+        points: 2,
+        text: 'Bacon',
+      },
+    ],
+    question: "Name something you'd only eat for breakfast",
+  },
+  '-MQiUstBzQpGgzZdEiqp': {
+    answers: [
+      {
+        points: 20,
+        text: 'America/ USA',
+      },
+      {
+        points: 16,
+        text: 'Australia',
+      },
+      {
+        points: 13,
+        text: 'Canada',
+      },
+      {
+        points: 8,
+        text: 'Africa',
+      },
+      {
+        points: 6,
+        text: 'Argentina',
+      },
+      {
+        points: 5,
+        text: 'Russia',
+      },
+      {
+        points: 4,
+        text: 'Albania',
+      },
+      {
+        points: 4,
+        text: 'China',
+      },
+    ],
+    question: 'Name a country ending in the letter "A"',
+  },
+  '-MQkY4xFw8ISqWSwcCFG': {
+    answers: [
+      {
+        points: 25,
+        text: 'Candlestick',
+      },
+      {
+        points: 19,
+        text: 'Knife',
+      },
+      {
+        points: 11,
+        text: 'Gun',
+      },
+      {
+        points: 9,
+        text: 'Rope',
+      },
+      {
+        points: 6,
+        text: 'Sword',
+      },
+    ],
+    question: 'Name one of the weapons from the game clue',
+  },
+  '-MQkYlm8pHui-F6PNza1': {
+    answers: [
+      {
+        points: 37,
+        text: 'Sandwich',
+      },
+      {
+        points: 22,
+        text: 'Apple ',
+      },
+      {
+        points: 19,
+        text: 'Juice Box/ Juice',
+      },
+      {
+        points: 6,
+        text: 'Thermos',
+      },
+      {
+        points: 5,
+        text: 'Cookies',
+      },
+      {
+        points: 5,
+        text: 'Fruit',
+      },
+      {
+        points: 4,
+        text: 'Chips',
+      },
+    ],
+    question: "Name something you might find in a school kid's lunch box",
+  },
 };
---
+++ src/services/firebaseService.ts
@@ -0,0 +1,40 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+import { child, get, getDatabase, onValue, ref } from 'firebase/database';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAPnhdmY4roxisTau_dZ8J4usqdX-mvVxI',
+  authDomain: 'ajs-playgrou.firebaseapp.com',
+  databaseURL: 'https://ajs-playgrou-default-rtdb.firebaseio.com',
+  projectId: 'ajs-playgrou',
+  storageBucket: 'ajs-playgrou.appspot.com',
+  messagingSenderId: '450339890811',
+  appId: '1:450339890811:web:eb632e9a40f7b3011f64de',
+  measurementId: 'G-FKHJ9T1N5M',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const dbRef = ref(getDatabase(app));
+export const questionsRef = child(dbRef, '/family-feud-v1/questions');
+
+export function foobar() {
+  console.log('/family-feud-v1/questions');
+  get(questionsRef)
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+      } else {
+        console.log('No data available');
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
