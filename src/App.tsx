1171bbf5d1f4bbd8d66bd866bd914d2b5699d959
AJ Almaguer <ajalmaguer@gmail.com>
2023-12-17 13:17:17 -0700

    can deploy to gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { createBrowserRouter, Link, RouterProvider } from 'react-router-dom';
+import { createHashRouter, Link, RouterProvider } from 'react-router-dom';
+import { BoardPage } from './pages/BoardPage';
+import {
+  PlayerQuestionDetailPage,
+  playerQuestionDetailsPagePath,
+} from './pages/PlayerQuestionDetailPage';
 import {
   QuestionDetailPage,
   questionDetailPagePath,
@@ -7,47 +12,39 @@ import {
   QuestionListPage,
   questionListPagePath,
 } from './pages/QuestionListPage';
-import {
-  PlayerQuestionDetailPage,
-  playerQuestionDetailsPagePath,
-} from './pages/PlayerQuestionDetailPage';
-import { BoardPage } from './pages/BoardPage';
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      errorElement: <div>Not found</div>,
-      element: (
-        <div>
-          <div>Family Feud</div>
-          <ul>
-            <li>
-              <Link to={questionListPagePath()}>Question List</Link>
-            </li>
-          </ul>
-        </div>
-      ),
-    },
-    {
-      path: questionListPagePath(),
-      element: <QuestionListPage />,
-    },
-    {
-      path: questionDetailPagePath(':id'),
-      element: <QuestionDetailPage />,
-    },
-    {
-      path: playerQuestionDetailsPagePath(':playerId', ':questionId'),
-      element: <PlayerQuestionDetailPage />,
-    },
-    {
-      path: 'board',
-      element: <BoardPage />,
-    },
-  ],
-  { basename: '/family-feud' }
-);
+const router = createHashRouter([
+  {
+    path: '/',
+    errorElement: <div>Not found</div>,
+    element: (
+      <div>
+        <div>Family Feud</div>
+        <ul>
+          <li>
+            <Link to={questionListPagePath()}>Question List</Link>
+          </li>
+        </ul>
+      </div>
+    ),
+  },
+  {
+    path: questionListPagePath(),
+    element: <QuestionListPage />,
+  },
+  {
+    path: questionDetailPagePath(':id'),
+    element: <QuestionDetailPage />,
+  },
+  {
+    path: playerQuestionDetailsPagePath(':playerId', ':questionId'),
+    element: <PlayerQuestionDetailPage />,
+  },
+  {
+    path: 'board',
+    element: <BoardPage />,
+  },
+]);
 
 export default function App() {
   return (
